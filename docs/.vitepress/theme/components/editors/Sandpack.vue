76349c823e4536fe893194a0e0e41857fbde74da
田建军 <145431125+TianJianJun0727@users.noreply.github.com>
2026-07-13 18:52:46 +0800

    fix(docs): avoid imported props type in sandpack wrapper (#93)
    Co-authored-by: 田建军 <tianjianjun@qipeng.com>

--- docs/.vitepress/theme/components/editors/Sandpack.vue
+++ docs/.vitepress/theme/components/editors/Sandpack.vue
@@ -1,9 +1,5 @@
 <script lang="ts" setup>
-import { type FunctionalComponent } from 'vue';
-import { type YCloudIconsProps } from '@ycloud-web/icons-vue';
 import { Sandpack } from 'sandpack-vue3';
-
-defineProps<FunctionalComponent<YCloudIconsProps>>();
 </script>
 
 <template>
